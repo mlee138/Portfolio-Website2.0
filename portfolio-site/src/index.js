d536db8309358e2abf49df28e380ce3fd5fff3c4
mlee138 <mlee138@binghamton.edu>
2019-10-25 22:19:44 -0400

    reworked Components to add colored navigation functionality

--- portfolio-site/src/index.js
+++ portfolio-site/src/index.js
@@ -1,57 +1,121 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-import './about-me.css'
+import './about.css'
 import './portfolio.css'
 import './resume.css'
 import './contact.css'
 
-class NavigationButtons extends React.Component{
+class App extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			currentContent: "about"
+		};
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(a) {
+		var newState = {};
+		newState["currentContent"] = a;
+		this.setState(newState);
+	}
+
+
+
 	render(){
+		return(
+		<div>
+			<nav>
+				<a id="icon" href="https://www.linkedin.com/in/matthewlee1297/" target="_blank" rel="noopener noreferrer"><img id="face-logo" src="Matt_Lee_Icon.ico" alt="Matt Lee Face Icon" /></a>
+				<NavigationButtons Id="desktop-nav" handleClick={this.handleClick}/>
+				<div id="dropdown">
+					<span>=</span>
+					<NavigationButtons Id="" handleClick={this.handleClick}/>
+				</div>
+			</nav>
+			<main>
+				<ChooseContent content={this.state.currentContent}/>
+			</main>
+		</div>
+		);
+	}
+}
+
+function ChooseContent(props){
+	const content = props.content;
+	switch(content){
+		case 'about':
+			return <About/>;
+		case "portfolio":
+			return <Portfolio/>;
+		case "resume":
+			return <Resume/>;
+		case "contact":
+			return <Contact/>;
+		default:
+			return;
+	}
+}
+
+class NavigationButtons extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = {
+			about: false,
+			portfolio: false,
+			resume: false,
+			contact: false
+		};
+
+		this.handleClickData = this.handleClickData.bind(this);
+	}
+
+	handleClickData(data){
+		var updateState = {};
+		updateState[data] = true;
+		this.setState(updateState);
+		this.props.handleClick(data);
+	}
+
+	render(){
+		var aboutClasses, portfolioClasses, resumeClasses, contactClasses;
+		(this.state.about ? aboutClasses='color' : aboutClasses=undefined);
+		(this.state.portfolio ? portfolioClasses='color' : portfolioClasses=undefined);
+		(this.state.resume ? resumeClasses='color' : resumeClasses=undefined);
+		(this.state.contact ? contactClasses='color' : contactClasses=undefined);
 		return(
 			<ul id={this.props.Id}>
 				<li>
-					<button className="about-me" 
-						onClick={()=> ReactDOM.render(<AboutMe />, document.getElementById('main'))}>
-						<img src="Background-1.jpg" alt="About me" />
+					<button	
+						onClick={this.handleClickData.bind(this, "about")}>
+						<img className={aboutClasses}  src="Background-1.jpg" alt="About me" />
 					</button></li>
 				<li>
-					<button className="portfolio"
-						onClick={()=> ReactDOM.render(<Portfolio />, document.getElementById('main'))}>
-						<img src="Background-2.jpg" alt="Portfolio" />
+					<button
+						onClick={this.handleClickData.bind(this, "portfolio")}>
+						<img className={portfolioClasses} src="Background-2.jpg" alt="Portfolio" />
 					</button></li>
 				<li>
-					<button className="resume"
-						onClick={()=> ReactDOM.render(<Resume />, document.getElementById('main'))}>
-						<img src="Background-3.jpg" alt="Resume" />
+					<button
+						onClick={this.handleClickData.bind(this, "resume")}>
+						<img className={resumeClasses} src="Background-3.jpg" alt="Resume" />
 					</button></li>
 				<li>
-					<button className="contact"
-						onClick={()=> ReactDOM.render(<Contact />, document.getElementById('main'))}>
-						<img src="Background-4.jpg" alt="Contact" />
+					<button
+						onClick={this.handleClickData.bind(this, "contact")}>
+						<img className={contactClasses} src="Background-4.jpg" alt="Contact" />
 					</button></li>
 			</ul>
 		);
 	}
 }
 
-class Navigation extends React.Component{
-	render(){
-		return(
-			<nav>
-				<a id="icon" href="https://www.linkedin.com/in/matthewlee1297/" target="_blank" rel="noopener noreferrer"><img id="face-logo" src="Matt_Lee_Icon.ico" alt="Matt Lee Face Icon" /></a>
-				<NavigationButtons Id="desktop-nav"/>
-				<div id="dropdown">
-					<span>=</span>
-					<NavigationButtons Id=""/>
-				</div>
-			</nav>
-		);
-	}
-}
-
 // ===========================================
-
+/*
 class Banner extends React.Component{
 	render(){
 		return(
@@ -59,13 +123,13 @@ class Banner extends React.Component{
 		);
 	}
 }
-
+*/
 // ===========================================
 
-class AboutMeSection extends React.Component{
+class AboutSection extends React.Component{
 	render(){
 		return(
-			<section className="about-me-section">
+			<section className="about-section">
 				<img src={this.props.image} alt={this.props.alt} />
 				<div className="overlay">
 					<p>{this.props.text}</p>
@@ -75,26 +139,26 @@ class AboutMeSection extends React.Component{
 	}
 }
 
-class AboutMe extends React.Component{
+class About extends React.Component{
 	render(){
 		return( 
-			<div id="about-me">
-				<div className="about-me-row">
-				<AboutMeSection 
+			<div id="about">
+				<div className="about-row">
+				<AboutSection 
 					image="images/Linkedin.jpg"
 					alt="Matt Lee, that's me"
 					text="Hi, my name is Matthew Lee. I've recently graduated from Binghamton University and am now back in New York City. Through clear, simple designs and pixel perfect work, I want to take my web development skills to the next level." />
-				<AboutMeSection 
+				<AboutSection 
 					image="images/apparel_1.png"
 					alt="Skills"
 					text=" Languages I am knowledgeable in includes HTML, CSS, JavaScript, and Java. I also have some experience with React, jQuery, Python, PHP, and SQL. Other skills include responsive design, Git, teamwork, and time management."/>
 				</div>
-				<div className="about-me-row">
-				<AboutMeSection 
+				<div className="about-row">
+				<AboutSection 
 					image="images/Plant.png"
 					alt="Hobbies"
 					text="I try to spread out my free time to my different hobbies. I'll create graphics with Photoshop and Illustrator, go hiking, watch the latest movie, or maybe play games. Doing these activities has helped me gain a strong appreciation for the arts and the process of creating large-scale projects."/>
-				<AboutMeSection 
+				<AboutSection 
 					image="images/Elli.png"
 					alt="My Cat Ellie"
 					text="This is my cat, Ellie! She's 3 years old and her interests include sunbathing, sitting in boxes, eating chicken, and chasing laser-pointers. She also enjoys the occassional catnip. &quot;Meow meow? Meow. Meow meow meow!&quot;"/>
@@ -117,7 +181,7 @@ class WebsiteCard extends React.Component{
 				<div className="card-text">
 					<h4>{this.props.title}</h4>
 					<p>{this.props.text}</p>
-					<a href={this.props.github} target="_blank">Check it out on Github</a>
+					<a href={this.props.github} target="_blank" rel="noopener noreferrer">Check it out on Github</a>
 				</div>
 			</div>
 		);
@@ -144,7 +208,7 @@ class GraphicCard extends React.Component{
 class MoreToCome extends React.Component{
 	render(){
 		return(
-			<div class="card more-to-come">
+			<div className="card more-to-come">
 				<p>MORE TO COME...</p>
 			</div>
 		);
@@ -214,7 +278,7 @@ class Resume extends React.Component{
 	render(){
 		return(
 			<div id="resume">
-		    	<a href="images/Matthew_Lee_Resume_Fall_2019.pdf" download>- Click here to download -</a>
+		    	<a href="images/Matthew_Lee_Resume_Fall_2019.pdf" rel="noopener noreferrer" download>- Click here to download -</a>
 		    	<br/>
 		    	<embed src="images/Matthew_Lee_Resume_Fall_2019.pdf"/>
 			</div>
@@ -228,7 +292,7 @@ class ContactMethod extends React.Component {
 	render() {
 		return(
 			<section>
-				<a href={this.props.link} target="_blank"><img src={this.props.image} alt={this.props.alt}/></a>
+				<a href={this.props.link} target="_blank" rel="noopener noreferrer"><img src={this.props.image} alt={this.props.alt}/></a>
 				<p>{this.props.text}</p>
 			</section>
 		);
@@ -259,4 +323,4 @@ class Contact extends React.Component{
 	}
 }
 
-ReactDOM.render(<Navigation />, document.getElementById('nav'));
+ReactDOM.render(<App />, document.getElementById('app'));
