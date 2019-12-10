c019eff3a6fecadeacfa00e9167e28cc4529860e
mlee138 <mlee138@binghamton.edu>
2019-12-10 12:02:59 -0500

    added image and changed css

--- src/pages/startup.js
+++ src/pages/startup.js
@@ -4,10 +4,11 @@ import './startup.css'
 function Startup(){
 	return (
 		<div id="startup">
+			<img src="images/self.jpg"/>
 			<h1><span>Matthew</span> Lee</h1>
 			<h2>Jr. Front-End Developer</h2>
 			<br/>
-			<label name="get started" htmlFor="about-btn">Get Started</label>
+			<label name="get started" htmlFor="about-btn"><span>Get Started</span></label>
 		</div>
 	);
 }
