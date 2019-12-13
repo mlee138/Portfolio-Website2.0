7352ac4a9a5a38af417005f0f5f0ed7062fe2a51
mlee138 <mlee138@binghamton.edu>
2019-12-13 16:16:41 -0500

    redid About section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,36 +1,80 @@
 import React from 'react';
 import './stylesheets/about.css';
 
-function AboutSection(props){
+function AboutSlide(props){
 	return(
-		<section className="about-section">
-			<img src={props.image} alt={props.alt} />
-			<div className="overlay">
+		<section className="slide">
+				<h1>{props.header}</h1>
+				<img src={props.image} alt={props.alt}/>
 				<p>{props.text}</p>
-			</div>
+				<blockquote>{props.extraText}</blockquote>
 		</section>
 	);
 }
 
-function AboutSlide(props){
-
-}
-
 class About extends React.Component{
+	constructor(props){
+		super();
+		this.state = {
+			color: 'color-start'
+		}
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick = (newColor) => {
+		this.setState({ color: newColor });
+	}
 	render(){
 		return( 
-			<div id="about">
-				<h2 className="banner">About Me</h2>
-				<p>I am a Jr. Front-End Developer, based in New York City, who is in love with designing and programming websites.</p>
-				<div className="about-row">
-					<AboutSection 
-						image="images/skills.jpg"
-						alt="Skills"
-						text="Skills: HTML, CSS, JavaScript, Java, jQuery, React, responsive design, Git, teamwork, and time management"/>
-					<AboutSection 
-						image="images/Ellie.jpg"
-						alt="My Cat Ellie"
-						text="This is my cat, Ellie! She's 3 years old and her interests include sunbathing, sitting in boxes, eating chicken, and chasing laser-pointers. She also enjoys the occassional catnip. &quot;Meow meow? Meow. Meow meow meow!&quot;"/>
+			<div id="slideshow">
+				<div id="labels" className={this.state.color}>
+					<label htmlFor='intro' id="intro-label" className='btn' onClick={() => this.handleClick("color-intro")}><h2>Intro</h2></label>
+					<hr/>
+					<label htmlFor='skills' id="skills-label" className='btn' onClick={() => this.handleClick("color-skills")}><h2>Skills</h2></label>
+					<hr/>
+					<label htmlFor='cat' id="cat-label" className='btn' onClick={() => this.handleClick("color-cat")}><h2>My Cat</h2></label>
+					<hr/>
+				</div>
+				<div>
+				<input id='intro' className='rad' name='rad' type='radio'/>
+				<AboutSlide
+					header="Intro"
+					image="../images/self.jpg"
+					alt="Matt Lee"
+					text="I am a Jr. Front-End Developer and a recent Binghamton University graduate. I'm based in New York City, and love with designing and building websites."/>
+
+				<input id='skills' className='rad' name='rad' type='radio'/>
+				<section className="slide">
+					<h1>Skills</h1>
+					<hr/>
+					<div id="lists">
+						<ul>
+							<li>HTML</li>
+							<li>CSS/SCSS</li>
+							<li>JavaScript</li>
+							<li>Java</li>
+						</ul>
+						<ul>
+							<li>jQuery</li>
+							<li>React</li>
+							<li>Responsive Design</li>
+							<li>Git</li>
+						</ul>
+						<ul>
+							<li>Time Management</li>
+							<li>Conflict Resolution</li>
+						</ul>
+					</div>	
+				</section>
+
+				<input id='cat' className='rad' name='rad' type='radio'/>
+				<AboutSlide
+					header="My Cat"
+					image="../images/Ellie.jpg"
+					alt="Ellie Cat"
+					text="This is my cat, Ellie! She's 3 years old and her interests include sunbathing, sitting in boxes, eating chicken, and chasing laser-pointers. She also enjoys the occassional catnip."
+					extraText="&quot;Meow meow? Meow. Meow meow meow!&quot;"/>
 				</div>
 			</div>
 		);
--- src/pages/startup.js
+++ src/pages/startup.js
@@ -4,9 +4,8 @@ import './stylesheets/startup.css'
 function Startup(){
 	return (
 		<div id="startup">
-			<img src="images/self.jpg"/>
 			<h1><span>Matthew</span> Lee</h1>
-			<h2>Jr. Front-End Developer</h2>
+			<h2>Jr. Web Developer</h2>
 			<br/>
 			<label name="get started" htmlFor="about-btn"><span>Get Started</span></label>
 		</div>
