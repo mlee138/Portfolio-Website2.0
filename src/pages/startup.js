6bce2e5cbe0a43d49e9cb9b0f2c1bfd2bdb9de85
mlee138 <mlee138@binghamton.edu>
2019-12-15 18:08:03 -0500

    added startup animation

--- src/pages/startup.js
+++ src/pages/startup.js
@@ -4,7 +4,10 @@ import './stylesheets/startup.css'
 function Startup(){
 	return (
 		<div id="startup">
-			<h1><span>Matthew</span> Lee</h1>
+			<svg height="80" width="400">
+				<text x="35" y="67%">Matthew Lee</text>
+			</svg>
+			
 			<h2>Jr. Web Developer</h2>
 			<br/>
 			<label name="get started" htmlFor="about-btn"><span>Get Started</span></label>
